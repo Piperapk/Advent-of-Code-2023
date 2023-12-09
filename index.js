cf15edc21d5802487c2e99af322de476715e3583
Piperapk <piperapk@gmail.com>
2023-12-09 02:26:00 +0000

    aoc day 1 complete

---
+++ index.js
@@ -0,0 +1,105 @@
+import fs from "fs";
+
+// advent day 1 part 1
+
+//const data = fs.readFileSync("./day1Input.txt", "utf-8").trim().split("\n");
+
+// let firstAndLastNumberForData = [];
+
+// function getNumberInDirection(string, direction = 'toLeft') {
+//   let stringArray = string.split('');
+//   if (direction === 'toRight') {
+//     stringArray.reverse();
+//   }
+//   let number;
+
+//   for (let character of stringArray) {
+//     if (Number(character)) {
+//       number = character;
+//       return number;
+//     }
+//   }
+// }
+
+// for (let string of data) {
+//   const combinedNumber =
+//     getNumberInDirection(string) + getNumberInDirection(string, 'toRight');
+
+//   firstAndLastNumberForData.push(Number(combinedNumber));
+// }
+
+// const finalResult = firstAndLastNumberForData.reduce(
+//   (accumulator, currentValue) => accumulator + currentValue,
+//   0
+// );
+
+// console.log(finalResult);
+
+// advent day 1 part 2
+
+const data = fs.readFileSync("./day1Input.txt", "utf-8").trim().split("\n");
+
+let firstAndLastNumberForData = [];
+
+function getNumberFromString(string) {
+  const numberMap = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  const numberFirstIndex = {};
+  const numberLastIndex = {};
+
+  // iterate over the number map and get the index of each key and value (numbers) in the map
+  for (let key of Object.keys(numberMap)) {
+    numberFirstIndex[key] = string.indexOf(key);
+    numberFirstIndex[numberMap[key]] = string.indexOf(numberMap[key]);
+    numberLastIndex[key] = string.lastIndexOf(key);
+    numberLastIndex[numberMap[key]] = string.lastIndexOf(numberMap[key]);
+  }
+
+  // get lowest and highest indices
+  const lowest = Object.entries(numberFirstIndex).reduce(
+    (accumulator, currentValue) => {
+      const [key, value] = currentValue;
+      if (accumulator[1] > value && value !== -1) {
+        accumulator[0] = !Number(key) ? numberMap[key] : key;
+        accumulator[1] = value;
+      }
+      return accumulator;
+    },
+    [0, string.length]
+  );
+  const highest = Object.entries(numberLastIndex).reduce(
+    (accumulator, currentValue) => {
+      const [key, value] = currentValue;
+      if (accumulator[1] <= value) {
+        accumulator[0] = !Number(key) ? numberMap[key] : key;
+        accumulator[1] = value;
+      }
+      return accumulator;
+    },
+    [0, 0]
+  );
+
+  return String(lowest[0]) + String(highest[0]);
+}
+
+for (let string of data) {
+  const combinedNumber = getNumberFromString(string);
+  firstAndLastNumberForData.push(Number(combinedNumber));
+}
+
+const finalResult = firstAndLastNumberForData.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  0
+);
+
+console.log(finalResult);
